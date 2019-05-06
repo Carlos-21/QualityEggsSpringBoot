4bb64963a91a30d99cb7145e5b88e94df0adfe01
Carlos-21 <carlos.llontop3@unmsm.edu.pe>
2019-05-06 11:30:41 -0500

    inicio

---
+++ src/main/java/com/unmsm/fisi/QualityEggsApplication.java
@@ -0,0 +1,13 @@
+package com.unmsm.fisi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityEggsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityEggsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unmsm/fisi/QualityEggsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.unmsm.fisi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QualityEggsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
