b705cf4aa276d636f40abb97930ef202fed75bd1
Carlos-21 <carlos.llontop3@unmsm.edu.pe>
2019-05-19 15:06:00 -0500

    Avance 1.5

---
+++ src/main/java/com/unmsm/fisi/configuration/ApplicationConfiguration.java
@@ -0,0 +1,11 @@
+package com.unmsm.fisi.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = { "com.unmsm.fisi.configuration"})
+public class ApplicationConfiguration
+{
+
+}
